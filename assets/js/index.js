81c019fede8be50d913087a1b41c88515b319e3a
Davide Malavenda <davide.malavenda26@gmail.com>
2024-07-26 11:22:59 +0200

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,6 @@
+fetch('https://striveschool-api.herokuapp.com/api/put-your-endpoint-here/', {
+  headers: {
+    Authorization:
+      'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NmEzNjc3NWYyNjBjYzAwMTVjYzBlMDAiLCJpYXQiOjE3MjE5ODQ4ODYsImV4cCI6MTcyMzE5NDQ4Nn0.HwAWXnMy17NTOce0k2hIkZsQG-H0NYIMAl_GnJVCm8g',
+  },
+})
